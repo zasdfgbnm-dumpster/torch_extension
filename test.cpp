e39245ff27a20759620c6877c78c7a2f5402d777
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-04-29 19:12:56 -0700

    save

--- test.cpp
+++ test.cpp
@@ -4,8 +4,7 @@
 #include <string>
 #include <algorithm>
 
-template <class CharT>
-class basic_string_view final {
+class string_view final {
  public:
   static constexpr int npos = int(-1);
 
@@ -13,20 +12,18 @@ class basic_string_view final {
     substr_(1).equals_(substr_(1));
     return 0;
   }
-  constexpr basic_string_view substr_(int pos = 0, int count = npos)
+  constexpr string_view substr_(int pos = 0, int count = npos)
       const {
-    return basic_string_view{begin_ + pos, std::min(count, size_ - pos)};
+    return string_view{begin_ + pos, std::min(count, size_ - pos)};
   }
 
-  constexpr bool equals_(basic_string_view rhs) const {
+  constexpr bool equals_(string_view rhs) const {
     return size_ == rhs.size_ && 0 == __builtin_memcmp(begin_, rhs.begin_, size_);
   }
 
   const char *begin_ = "12345";
   int size_ = 5;
 };
-
-using string_view = basic_string_view<char>;
 
 int main() {
   constexpr auto x = string_view().find();
