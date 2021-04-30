719f903971b8d1f58c8e71986b9f770d288a19ee
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-04-29 19:18:23 -0700

    save

--- test.cu
+++ test.cu
@@ -8,9 +8,6 @@ class string_view final {
  public:
   static constexpr int npos = int(-1);
 
-  constexpr bool test() const noexcept {
-    return substr_(1).equals_(substr_(1));
-  }
   constexpr string_view substr_(int pos = 0, int count = npos)
       const {
     return string_view{begin_ + pos, std::min(count, size_ - pos)};
@@ -18,6 +15,10 @@ class string_view final {
 
   constexpr bool equals_(string_view rhs) const {
     return size_ == rhs.size_ && 0 == __builtin_memcmp(begin_, rhs.begin_, size_);
+  }
+
+  constexpr bool test() const noexcept {
+    return substr_(1).equals_(substr_(1));
   }
 
   const char *begin_ = "12345";
