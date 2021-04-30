c67f080554cb156eeffa9a38f52a61bead6f773e
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-04-29 19:06:07 -0700

    save

--- test.cpp
+++ test.cpp
@@ -27,34 +27,9 @@ class basic_string_view final {
   /* implicit */ constexpr basic_string_view(const_pointer str)
       : basic_string_view(str, strlen_(str)) {}
 
-  /* implicit */ basic_string_view(const ::std::basic_string<CharT>& str)
-      : basic_string_view(str.data(), str.size()) {}
-
-  constexpr basic_string_view(const basic_string_view&) noexcept = default;
-
-  constexpr const_pointer data() const noexcept {
-    return begin_;
-  }
-
-  constexpr size_type size() const noexcept {
-    return size_;
-  }
-
-  constexpr size_type find(basic_string_view v, size_type pos = 0) const
-      noexcept {
-    if (v.size() == 0) {
-      return pos <= size() ? pos : npos;
-    }
-
-    if (pos + v.size() <= size()) {
-      for (size_type cur = pos, end = size() - v.size(); cur <= end; ++cur) {
-        if (v.at_(0) == at_(cur) &&
-            v.substr_(1).equals_(substr_(cur + 1, v.size() - 1))) {
-          return cur;
-        }
-      }
-    }
-    return npos;
+  constexpr size_type find() const noexcept {
+    substr_(1).equals_(substr_(1));
+    return 0;
   }
 
   constexpr size_type find(const_pointer s, size_type pos = 0) const {
@@ -62,7 +37,6 @@ class basic_string_view final {
   }
 
   static constexpr size_type strlen_(const_pointer str) noexcept {
-    // if we are in C++14, write it iteratively. This is faster.
     const_pointer current = str;
     while (*current != '\0') {
       ++current;
@@ -70,28 +44,21 @@ class basic_string_view final {
     return current - str;
   }
 
-  constexpr const_reference at_(size_type pos) const noexcept {
-    return *(begin_ + pos);
-  }
-
   constexpr basic_string_view substr_(size_type pos = 0, size_type count = npos)
       const {
-    return basic_string_view{begin_ + pos, std::min(count, size() - pos)};
+    return basic_string_view{begin_ + pos, std::min(count, size_ - pos)};
   }
 
   constexpr bool equals_(basic_string_view rhs) const {
-    return size() == rhs.size() && 0 == __builtin_memcmp(data(), rhs.data(), size());
+    return size_ == rhs.size_ && 0 == __builtin_memcmp(begin_, rhs.begin_, size_);
   }
 
   const_pointer begin_;
   size_type size_;
 };
 
-template <class CharT>
-const typename basic_string_view<CharT>::size_type basic_string_view<CharT>::npos;
-
 using string_view = basic_string_view<char>;
 
 int main() {
-  constexpr auto x = string_view("fsdfa::fsdfas").find("fff");
+  constexpr auto x = string_view("fsdfa::fsdfas").find();
 }
