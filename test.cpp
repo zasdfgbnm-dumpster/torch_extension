35c0fb8ca70776ec2a600dcf7c339816ebb4c5d2
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-05-10 10:07:32 -0700

    save

--- test.cpp
+++ test.cpp
@@ -1,16 +1,13 @@
 template <class CurClass>
-class class_;
-
-class Library final {
-public:
-  template <class CurClass>
-  inline class_<CurClass> class_() {}
-};
-
-template <class CurClass>
 class class_ {};
 
-struct CuaevComputer {};
+class Library {
+public:
+  template <class CurClass>
+  inline class_<CurClass> class_() { return {}; }
+};
+
+class CuaevComputer {};
 
 int main() {
   Library().class_<CuaevComputer>();
